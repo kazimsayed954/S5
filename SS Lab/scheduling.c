d6d0b4be4dce61c2e76e05aabacfc90876dc85d6
SrividyaKK <SrividyaKK@users.noreply.github.com>
2019-08-19 00:06:42 +0530

    slight modifications

--- SS Lab/scheduling.c
+++ SS Lab/scheduling.c
@@ -13,8 +13,7 @@ void main()
     printf("Enter no. of processes: ");
     scanf("%d", &n);
     p = (struct process *)calloc(n, sizeof(struct process));
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++) {
         (p + i)->pname = i + 1;
         printf("Enter the arrival time, burst time and priority of process %d\n", i + 1);
         scanf("%d %d %d", &(p + i)->arrival, &(p + i)->burst, &(p + i)->priority);
@@ -27,12 +26,9 @@ void main()
     switch (c)
     {
     case 1: //sort based on arrival time
-        for (i = 0; i < n; i++)
-        {
-            for (j = 0; j < n - i - 1; j++)
-            {
-                if ((p + j)->arrival > (p + j + 1)->arrival)
-                {
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n - i - 1; j++) {
+                if ((p + j)->arrival > (p + j + 1)->arrival) {
                     temp = *(p + j);
                     *(p + j) = *(p + j + 1);
                     *(p + j + 1) = temp;
@@ -40,8 +36,7 @@ void main()
             }
         }
         printf("Process\tArrival time\tBurst time\tWaiting time\tTurnaround time\n");
-        for (i = 0; i < n; i++)
-        {
+        for (i = 0; i < n; i++) {
             (p + i)->wt = tot_time - (p + i)->arrival;
             tot_time += (p + i)->burst; //process executes
             (p + i)->tat = (p + i)->wt + (p + i)->burst;
@@ -52,12 +47,9 @@ void main()
         printf("Average waiting time = %f\nAverage turnaround time = %f\n", tot_wt / n, tot_tat / n);
         break;
     case 2: //sort based on burst time
-        for (i = 0; i < n; i++)
-        {
-            for (j = 0; j < n - i - 1; j++)
-            {
-                if ((p + j)->burst > (p + j + 1)->burst)
-                {
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n - i - 1; j++) {
+                if ((p + j)->burst > (p + j + 1)->burst) {
                     temp = *(p + j);
                     *(p + j) = *(p + j + 1);
                     *(p + j + 1) = temp;
@@ -68,8 +60,7 @@ void main()
         i = 0;
         while (count < n)
         {
-            if ((p + i)->arrival <= tot_time)
-            {
+            if ((p + i)->arrival <= tot_time) {
                 (p + i)->wt = tot_time - (p + i)->arrival;
                 tot_time += (p + i)->burst; //process executes
                 (p + i)->tat = (p + i)->wt + (p + i)->burst;
@@ -79,8 +70,7 @@ void main()
                 (p + i)->done = 1;
                 count++;
                 i = 0;
-                while (i < n)
-                {
+                while (i < n) {
                     if ((p + i)->done == 0)
                         break;
                     i++;
@@ -94,10 +84,8 @@ void main()
         break;
 
     case 3: //sort based on priority
-        for (i = 0; i < n; i++)
-        {
-            for (j = 0; j < n - i - 1; j++)
-            {
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n - i - 1; j++) {
                 if ((p + j)->priority > (p + j + 1)->priority || ((p + j)->priority == (p + j + 1)->priority && (p + j)->arrival > (p + j + 1)->arrival))
                 {
                     temp = *(p + j);
@@ -110,8 +98,7 @@ void main()
         i = 0;
         while (count < n)
         {
-            if ((p + i)->arrival <= tot_time)
-            {
+            if ((p + i)->arrival <= tot_time) {
                 (p + i)->wt = tot_time - (p + i)->arrival;
                 tot_time += (p + i)->burst; //process executes
                 (p + i)->tat = (p + i)->wt + (p + i)->burst;
@@ -139,12 +126,9 @@ void main()
         printf("Enter the time quantum: ");
         scanf("%d", &q);
         //sort based on arrival time
-        for (i = 0; i < n; i++)
-        {
-            for (j = 0; j < n - i - 1; j++)
-            {
-                if ((p + j)->arrival > (p + j + 1)->arrival)
-                {
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n - i - 1; j++) {
+                if ((p + j)->arrival > (p + j + 1)->arrival) {
                     temp = *(p + j);
                     *(p + j) = *(p + j + 1);
                     *(p + j + 1) = temp;
@@ -155,15 +139,12 @@ void main()
         i = 0;
         while (count < n)
         {
-            if ((p + i)->remaining != 0)
-            {
-                if (q <= (p + i)->remaining)
-                {
+            if ((p + i)->remaining != 0) {
+                if (q <= (p + i)->remaining) {
                     tot_time += q;
                     (p + i)->remaining -= q; //process executes 1 q
                 }
-                else
-                {
+                else {
                     tot_time += (p + i)->remaining;
                     (p + i)->remaining = 0;
                 }
