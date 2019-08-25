73a3fe0335c153a9f893bbe09ff52044f5d74907
SrividyaKK <srividya.krishnakumar99@gmail.com>
2019-08-25 17:39:31 +0000

    hierarchical dir

---
+++ SS Lab/hierarchical.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void main()
+{
+	int ch;
+	char d[15], f[15];
+	printf("1. Create directory. \n2. Create file. \n3. Delete File. \n4. Search for directory. \n5. Search for file. \n6. Delete directory. \n7. Display. \n8. Exit. \nENTER CHOICE: ");
+	scanf("%d", &ch);
+	switch (ch)
+	{
+	case 1: // create directory
+		break;
+	case 2: // create file
+		break;
+	case 3: // delete file
+		break;
+	case 4: // search for directory
+		break;
+	case 5: // search for file
+		break;
+	case 6: // delete directory
+		break;
+	case 7: // display
+		break;
+	default:
+		exit(0);
+	}
+}
